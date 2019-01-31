83f9d8cdd91031f823d540f0e27a4c0dc0699938
Michael Soares <mikey.soares@gmail.com>
2019-01-31 16:20:41 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,26 +1,21 @@
 class MusicImporter
+
 attr_accessor :path
 
   def initialize(path)
     @path = path
-
   end
 
   def files
-
   Dir.chdir(@path) do | path |
           Dir.glob("*.mp3")
       end
-
   end
-
 
   def import
     @file_import = self.files
     @file_import.each do |song|
       Song.create_from_filename(song)
-
+    end
   end
-end
-
 end
