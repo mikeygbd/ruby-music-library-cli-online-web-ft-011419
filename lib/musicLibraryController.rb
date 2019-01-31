e895adde8b69071b44eadf1b0ef00a41d8361e56
Michael Soares <mikey.soares@gmail.com>
2019-01-31 16:16:58 +0000

    Automatically backed up by Learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -21,21 +21,22 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.chomp
-    # input = gets.chomp
-    if input == "list songs"
-      list_songs
-    elsif input == "list artists"
-      list_artists
-    elsif input == "list genres"
-      list_genres
-    elsif input == "list artist"
-      list_songs_by_artist
-    elsif input == "list genre"
-      list_songs_by_genre
-    elsif input == "play song"
-      play_song
-      end
-    end
+    triggers
+    # # input = gets.chomp
+    # if input == "list songs"
+    #   list_songs
+    # elsif input == "list artists"
+    #   list_artists
+    # elsif input == "list genres"
+    #   list_genres
+    # elsif input == "list artist"
+    #   list_songs_by_artist
+    # elsif input == "list genre"
+    #   list_songs_by_genre
+    # elsif input == "play song"
+    #   play_song
+    #   end
+    # end
   end
 
   def list_songs
@@ -83,19 +84,6 @@ class MusicLibraryController
     end
   end
 end
-
-  # def play_song
-  #   song_array = []
-  #    list_songs.each do |song|
-  #      song_array << song
-  #   puts "Which song number would you like to play?"
-  #   input = gets.chomp.to_i
-  #   if input > 0 && input <= song_array.size
-  #     song_choice = song_list[input - 1]
-  #     end
-  #   end
-  # end
-
 
     def play_song
       puts "Which song number would you like to play?"
